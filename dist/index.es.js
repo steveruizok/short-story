58d87f6c11a5b705322ea8138d24e0a09f2e683d
Stephen Ruiz <steveruizok@gmail.com>
2018-09-21 00:12:13 +0200

    bringing this one back

--- dist/index.es.js
+++ dist/index.es.js
@@ -80,12 +80,323 @@ var possibleConstructorReturn = function (self, call) {
   return call && (typeof call === "object" || typeof call === "function") ? call : self;
 };
 
+<<<<<<< HEAD
 var ExampleComponent = function (_Component) {
   inherits(ExampleComponent, _Component);
 
   function ExampleComponent() {
     classCallCheck(this, ExampleComponent);
     return possibleConstructorReturn(this, (ExampleComponent.__proto__ || Object.getPrototypeOf(ExampleComponent)).apply(this, arguments));
+=======
+var theme = {
+  white: "#fff",
+  background: "#f6f6f6",
+  gray: "#eee",
+  black: "#000",
+  labelColor: "#555",
+  inputColor: "#777",
+  outerBorder: "1px solid #ccc",
+  border: "1px solid #eee",
+  rowPadding: "16px"
+};
+
+var ShortStory = function (_React$Component) {
+  inherits(ShortStory, _React$Component);
+
+  function ShortStory(props) {
+    classCallCheck(this, ShortStory);
+
+    var _this = possibleConstructorReturn(this, (ShortStory.__proto__ || Object.getPrototypeOf(ShortStory)).call(this, props));
+
+    _this.createKnob = function (knob) {
+      if (knob.hidden !== undefined) {
+        if (knob.hidden(_this.state)) {
+          return;
+        }
+      }
+
+      var input = _this.createKnobInput(knob);
+
+      return [React.createElement(
+        "div",
+        {
+          key: "Label_Box_" + knob.name,
+          style: {
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+            gridColumn: 1,
+            padding: theme.rowPadding,
+            borderTop: theme.border,
+            color: theme.labelColor
+          }
+        },
+        knob.label
+      ), React.createElement(
+        "div",
+        {
+          key: "Knob_Input" + knob.name,
+          style: {
+            gridColumn: 2,
+            padding: theme.rowPadding,
+            borderTop: theme.border,
+            color: theme.inputColor
+          }
+        },
+        input
+      )];
+    };
+
+    _this.createKnobInput = function (knob) {
+      var i = 0;
+
+      // let ref = React.createRef();
+      var key = knob.name;
+
+      var handleChange = throttle(function (ev) {
+        _this.setState(defineProperty({}, knob.name, ev.target.value));
+      }, 250);
+
+      var currentValue = _this.state[knob.name];
+
+      switch (knob.type) {
+        // TEXT
+        case "text":
+          return React.createElement(Input, {
+            key: key,
+            onChange: handleChange,
+            defaultValue: currentValue,
+            style: {
+              width: "calc(100% - 16px)",
+              fontFamily: "sans-serif",
+              fontSize: ".9em",
+              padding: "8px",
+              border: "1px solid #aaa"
+            }
+          });
+        // TEXTAREA
+        case "textarea":
+          return React.createElement("textarea", {
+            key: key,
+            onChange: handleChange,
+            defaultValue: currentValue,
+            style: {
+              width: "calc(100% - 16px)",
+              border: "1px solid #aaa",
+              fontFamily: "sans-serif",
+              fontSize: ".9em",
+              padding: "8px"
+            }
+          });
+        // DATE
+        case "date":
+          return React.createElement(Input, {
+            key: key,
+            type: "date",
+            min: knob.min,
+            max: knob.max,
+            onChange: handleChange,
+            value: currentValue,
+            style: {
+              border: "1px solid #aaa",
+              fontFamily: "sans-serif",
+              fontSize: ".9em",
+              padding: "8px"
+            }
+          });
+        // NUMBER
+        case "number":
+          return [React.createElement(
+            "div",
+            {
+              key: key + "_slider_currentValue",
+              style: {
+                width: "calc(100% - 16px)",
+                textAlign: "center"
+              }
+            },
+            React.createElement(
+              "small",
+              { key: key + "_slider_max" },
+              currentValue
+            )
+          ), React.createElement(Input, {
+            key: key,
+            type: "range",
+            min: knob.min,
+            max: knob.max,
+            onChange: handleChange,
+            defaultValue: currentValue,
+            style: {
+              width: "calc(100% - 16px)"
+            }
+          }), React.createElement(
+            "div",
+            {
+              key: key + "_slider_values",
+              style: {
+                display: "flex",
+                width: "calc(100% - 16px)",
+                justifyContent: "space-between",
+                fontFamily: "sans-serif",
+                fontSize: ".9em"
+              }
+            },
+            React.createElement(
+              "span",
+              { key: key + "_slider_min" },
+              knob.min
+            ),
+            React.createElement(
+              "span",
+              { key: key + "_slider_max" },
+              knob.max
+            )
+          )];
+        // ENUMERATED VALUE
+        case "enum":
+          return React.createElement(
+            "select",
+            {
+              key: key,
+              type: "select",
+              onChange: handleChange,
+              defaultValue: currentValue,
+              style: {
+                width: "calc(100% - 16px)",
+                fontFamily: "sans-serif",
+                fontSize: ".9em",
+                padding: "8px"
+              }
+            },
+            knob.options.map(function (o, index) {
+              return React.createElement(
+                "option",
+                { key: key + "_option_" + index, value: o },
+                knob.labels[index]
+              );
+            })
+          );
+        // SEGMENTED ENUMERATED VALUE
+        case "segment":
+          i++;
+          return React.createElement(
+            "div",
+            {
+              key: key + i + "_segment",
+              style: {
+                width: "calc(100% - 16px)"
+              }
+            },
+            knob.options.map(function (o, index) {
+              return [React.createElement(Input, {
+                key: key + i + "_SegmentOption_" + index,
+                type: "radio",
+                checked: currentValue === o,
+                mr: 2,
+                id: key + i + "_SegmentOption_" + index,
+                onChange: function onChange(ev) {
+                  _this.setState(defineProperty({}, knob.name, o));
+                },
+                style: {
+                  marginRight: "8px"
+                }
+              }), React.createElement(
+                "label",
+                {
+                  style: {
+                    marginRight: "16px"
+                  },
+                  key: key + i + "_SegmentOptionLabel_" + index,
+                  htmlFor: key + i + "_SegmentOption_" + index
+                },
+                knob.labels[index]
+              )];
+            })
+          );
+        // BOOLEAN
+        case "boolean":
+          return React.createElement(Input, {
+            key: key,
+            type: "checkbox",
+            onClick: function onClick(ev) {
+              _this.setState(defineProperty({}, knob.name, !currentValue));
+            },
+            defaultChecked: currentValue,
+            style: {}
+          });
+        // COLOR
+        case "color":
+          return [React.createElement(Input, {
+            key: key,
+            type: "color",
+            name: knob.name,
+            onChange: handleChange,
+            value: currentValue,
+            style: {
+              padding: 0,
+              margin: 0,
+              borderWidth: 0,
+              height: "32px",
+              width: "56px",
+              borderColor: "none",
+              backgroundColor: "none"
+            }
+          })];
+        // TIME
+        case "time":
+          return [React.createElement(Input, {
+            key: key,
+            type: "time",
+            min: knob.min,
+            max: knob.max,
+            onChange: handleChange,
+            value: currentValue,
+            style: {
+              border: "1px solid #aaa",
+              fontFamily: "sans-serif",
+              fontSize: ".9em",
+              padding: "8px"
+            }
+          })];
+        // TODO: IMAGE
+        default:
+          return React.createElement(
+            "span",
+            null,
+            "Nothing for that type."
+          );
+      }
+    };
+
+    _this.updateWidth = function (ev) {
+      if (!_this.measure.current) {
+        return;
+      }
+
+      var node = _this.measure.current.childNodes[0];
+
+      if (!node || node.offsetWidth === _this.state.width) {
+        return;
+      }
+
+      _this.setState({ width: node.offsetWidth });
+    };
+
+    _this.measure = React.createRef();
+
+    _this.state = {
+      width: 0,
+      activeTab: 0
+    };
+
+    Object.keys(_this.props.knobs).map(function (key) {
+      var knob = _this.props.knobs[key];
+      knob.name = key;
+      _this.state = _extends({}, _this.state, defineProperty({}, key, knob.default));
+    });
+    return _this;
+>>>>>>> parent of 195041b... short story
   }
 
   createClass(ExampleComponent, [{
@@ -93,6 +404,7 @@ var ExampleComponent = function (_Component) {
     value: function render() {
       var text = this.props.text;
 
+<<<<<<< HEAD
 
       return React.createElement(
         'div',
@@ -100,6 +412,114 @@ var ExampleComponent = function (_Component) {
         'Example Component: ',
         text
       );
+=======
+      var name = this.props.name;
+      return [React.createElement(
+        "div",
+        {
+          key: "stsy_" + name + "_knobs_heading_container",
+          style: {
+            all: "initial"
+          }
+        },
+        React.createElement(
+          "h2",
+          {
+            style: {
+              fontFamily: "sans-serif",
+              fontSize: "1.68em"
+            },
+            key: "stsy_" + name + "_heading"
+          },
+          name
+        )
+      ), React.createElement(
+        "div",
+        {
+          key: "stsy_" + name + "_component",
+          style: {
+            display: "flex",
+            border: theme.outerBorder,
+            borderBottom: "none",
+            borderRadius: "4px 4px 0 0",
+            backgroundColor: theme.background,
+            width: "auto",
+            justifyContent: "center"
+          }
+        },
+        React.createElement(
+          "div",
+          { key: "stsy_" + name + "_padding", style: { padding: "24px 0" } },
+          React.createElement(
+            "div",
+            {
+              key: "stsy_" + name + "_measure",
+              style: {
+                borderBottom: "1px solid #bbb",
+                backgroundColor: "#eee"
+              },
+              ref: this.measure
+            },
+            this.props.children(this.state)
+          ),
+          " ",
+          React.createElement(
+            "div",
+            {
+              style: {
+                all: "initial"
+              }
+            },
+            React.createElement(
+              "div",
+              {
+                key: "stsy_" + name + "_width",
+                style: {
+                  fontFamily: "sans-serif",
+                  fontSize: ".8em",
+                  color: "#777",
+                  paddingTop: "8px",
+                  textAlign: "center"
+                }
+              },
+              this.state.width,
+              "px"
+            )
+          )
+        )
+      ), React.createElement(
+        "div",
+        { key: "stsy_" + name + "_knobs_container", style: { all: "initial" } },
+        React.createElement(
+          "div",
+          {
+            key: "stsy_" + name + "_knobs_panel",
+            style: {
+              fontFamily: "sans-serif",
+              fontSize: ".9em",
+              margin: theme.margin,
+              border: theme.outerBorder,
+              borderRadius: "0 0 4px 4px",
+              overflow: "hidden",
+              marginBottom: "40px"
+            }
+          },
+          React.createElement(
+            "div",
+            {
+              key: "stsy_" + name + "_knobs_container",
+              style: {
+                display: "grid",
+                gridTemplateColumns: "25% auto"
+              }
+            },
+            Object.values(this.props.knobs).map(function (v) {
+              return _this2.createKnob(v);
+            })
+          )
+        )
+      )];
+>>>>>>> parent of 195041b... short story
     }
   }]);
   return ExampleComponent;
@@ -109,5 +529,13 @@ ExampleComponent.propTypes = {
   text: PropTypes.string
 };
 
+<<<<<<< HEAD
 export default ExampleComponent;
+=======
+var Input = function Input(props) {
+  return React.createElement("input", _extends({ key: "Stsy-input" }, props));
+};
+
+export default ShortStory;
+>>>>>>> parent of 195041b... short story
 //# sourceMappingURL=index.es.js.map
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
+<<<<<<< HEAD
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
 export default class ExampleComponent extends Component {
+=======
+import React from "react";
+import PropTypes from "prop-types";
+import { throttle } from "./utils";
+
+const theme = {
+  white: "#fff",
+  background: "#f6f6f6",
+  gray: "#eee",
+  black: "#000",
+  labelColor: "#555",
+  inputColor: "#777",
+  outerBorder: "1px solid #ccc",
+  border: "1px solid #eee",
+  rowPadding: "16px"
+};
+
+class ShortStory extends React.Component {
+>>>>>>> parent of 195041b... short story
   static propTypes = {
     text: PropTypes.string
   }
 
+<<<<<<< HEAD
   render() {
     const {
       text
@@ -16,7 +37,385 @@ export default class ExampleComponent extends Component {
     return (
       <div className={styles.test}>
         Example Component: {text}
+=======
+  // Create knobs
+  createKnob = knob => {
+    if (knob.hidden !== undefined) {
+      if (knob.hidden(this.state)) {
+        return;
+      }
+    }
+
+    const input = this.createKnobInput(knob);
+
+    return [
+      <div
+        key={"Label_Box_" + knob.name}
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+          gridColumn: 1,
+          padding: theme.rowPadding,
+          borderTop: theme.border,
+          color: theme.labelColor
+        }}
+      >
+        {knob.label}
+      </div>,
+      <div
+        key={"Knob_Input" + knob.name}
+        style={{
+          gridColumn: 2,
+          padding: theme.rowPadding,
+          borderTop: theme.border,
+          color: theme.inputColor
+        }}
+      >
+        {input}
+      </div>
+    ];
+  };
+
+  // Create the correct input for the knob type
+  createKnobInput = knob => {
+    let i = 0;
+
+    // let ref = React.createRef();
+    const key = knob.name;
+
+    const handleChange = throttle(ev => {
+      this.setState({ [knob.name]: ev.target.value });
+    }, 250);
+
+    const currentValue = this.state[knob.name];
+
+    switch (knob.type) {
+      // TEXT
+      case "text":
+        return (
+          <Input
+            key={key}
+            onChange={handleChange}
+            defaultValue={currentValue}
+            style={{
+              width: "calc(100% - 16px)",
+              fontFamily: "sans-serif",
+              fontSize: ".9em",
+              padding: "8px",
+              border: "1px solid #aaa"
+            }}
+          />
+        );
+      // TEXTAREA
+      case "textarea":
+        return (
+          <textarea
+            key={key}
+            onChange={handleChange}
+            defaultValue={currentValue}
+            style={{
+              width: "calc(100% - 16px)",
+              border: "1px solid #aaa",
+              fontFamily: "sans-serif",
+              fontSize: ".9em",
+              padding: "8px"
+            }}
+          />
+        );
+      // DATE
+      case "date":
+        return (
+          <Input
+            key={key}
+            type="date"
+            min={knob.min}
+            max={knob.max}
+            onChange={handleChange}
+            value={currentValue}
+            style={{
+              border: "1px solid #aaa",
+              fontFamily: "sans-serif",
+              fontSize: ".9em",
+              padding: "8px"
+            }}
+          />
+        );
+      // NUMBER
+      case "number":
+        return [
+          <div
+            key={key + "_slider_currentValue"}
+            style={{
+              width: "calc(100% - 16px)",
+              textAlign: "center"
+            }}
+          >
+            <small key={key + "_slider_max"}>{currentValue}</small>
+          </div>,
+          <Input
+            key={key}
+            type="range"
+            min={knob.min}
+            max={knob.max}
+            onChange={handleChange}
+            defaultValue={currentValue}
+            style={{
+              width: "calc(100% - 16px)"
+            }}
+          />,
+          <div
+            key={key + "_slider_values"}
+            style={{
+              display: "flex",
+              width: "calc(100% - 16px)",
+              justifyContent: "space-between",
+              fontFamily: "sans-serif",
+              fontSize: ".9em"
+            }}
+          >
+            <span key={key + "_slider_min"}>{knob.min}</span>
+            <span key={key + "_slider_max"}>{knob.max}</span>
+          </div>
+        ];
+      // ENUMERATED VALUE
+      case "enum":
+        return (
+          <select
+            key={key}
+            type="select"
+            onChange={handleChange}
+            defaultValue={currentValue}
+            style={{
+              width: "calc(100% - 16px)",
+              fontFamily: "sans-serif",
+              fontSize: ".9em",
+              padding: "8px"
+            }}
+          >
+            {knob.options.map((o, index) => (
+              <option key={key + "_option_" + index} value={o}>
+                {knob.labels[index]}
+              </option>
+            ))}
+          </select>
+        );
+      // SEGMENTED ENUMERATED VALUE
+      case "segment":
+        i++;
+        return (
+          <div
+            key={key + i + "_segment"}
+            style={{
+              width: "calc(100% - 16px)"
+            }}
+          >
+            {knob.options.map((o, index) => [
+              <Input
+                key={key + i + "_SegmentOption_" + index}
+                type="radio"
+                checked={currentValue === o}
+                mr={2}
+                id={key + i + "_SegmentOption_" + index}
+                onChange={ev => {
+                  this.setState({ [knob.name]: o });
+                }}
+                style={{
+                  marginRight: "8px"
+                }}
+              />,
+              <label
+                style={{
+                  marginRight: "16px"
+                }}
+                key={key + i + "_SegmentOptionLabel_" + index}
+                htmlFor={key + i + "_SegmentOption_" + index}
+              >
+                {knob.labels[index]}
+              </label>
+            ])}
+          </div>
+        );
+      // BOOLEAN
+      case "boolean":
+        return (
+          <Input
+            key={key}
+            type="checkbox"
+            onClick={ev => {
+              this.setState({ [knob.name]: !currentValue });
+            }}
+            defaultChecked={currentValue}
+            style={{}}
+          />
+        );
+      // COLOR
+      case "color":
+        return [
+          <Input
+            key={key}
+            type="color"
+            name={knob.name}
+            onChange={handleChange}
+            value={currentValue}
+            style={{
+              padding: 0,
+              margin: 0,
+              borderWidth: 0,
+              height: "32px",
+              width: "56px",
+              borderColor: "none",
+              backgroundColor: "none"
+            }}
+          />
+        ];
+      // TIME
+      case "time":
+        return [
+          <Input
+            key={key}
+            type="time"
+            min={knob.min}
+            max={knob.max}
+            onChange={handleChange}
+            value={currentValue}
+            style={{
+              border: "1px solid #aaa",
+              fontFamily: "sans-serif",
+              fontSize: ".9em",
+              padding: "8px"
+            }}
+          />
+        ];
+      // TODO: IMAGE
+      default:
+        return <span>Nothing for that type.</span>;
+    }
+  };
+
+  componentDidMount() {
+    this.updateWidth();
+    window.addEventListener("resize", throttle(this.updateWidth, 100));
+  }
+
+  componentDidUpdate() {
+    this.updateWidth();
+  }
+
+  updateWidth = ev => {
+    if (!this.measure.current) {
+      return;
+    }
+
+    const node = this.measure.current.childNodes[0];
+
+    if (!node || node.offsetWidth === this.state.width) {
+      return;
+    }
+
+    this.setState({ width: node.offsetWidth });
+  };
+
+  render() {
+    let name = this.props.name;
+    return [
+      <div
+        key={`stsy_${name}_knobs_heading_container`}
+        style={{
+          all: "initial"
+        }}
+      >
+        <h2
+          style={{
+            fontFamily: "sans-serif",
+            fontSize: "1.68em"
+          }}
+          key={`stsy_${name}_heading`}
+        >
+          {name}
+        </h2>
+      </div>,
+      <div
+        key={`stsy_${name}_component`}
+        style={{
+          display: "flex",
+          border: theme.outerBorder,
+          borderBottom: "none",
+          borderRadius: "4px 4px 0 0",
+          backgroundColor: theme.background,
+          width: "auto",
+          justifyContent: "center"
+        }}
+      >
+        <div key={`stsy_${name}_padding`} style={{ padding: "24px 0" }}>
+          <div
+            key={`stsy_${name}_measure`}
+            style={{
+              borderBottom: `1px solid #bbb`,
+              backgroundColor: "#eee"
+            }}
+            ref={this.measure}
+          >
+            {this.props.children(this.state)}
+          </div>{" "}
+          <div
+            style={{
+              all: "initial"
+            }}
+          >
+            <div
+              key={`stsy_${name}_width`}
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: ".8em",
+                color: "#777",
+                paddingTop: "8px",
+                textAlign: "center"
+              }}
+            >
+              {this.state.width}
+              px
+            </div>
+          </div>
+        </div>
+      </div>,
+      <div key={`stsy_${name}_knobs_container`} style={{ all: "initial" }}>
+        <div
+          key={`stsy_${name}_knobs_panel`}
+          style={{
+            fontFamily: "sans-serif",
+            fontSize: ".9em",
+            margin: theme.margin,
+            border: theme.outerBorder,
+            borderRadius: "0 0 4px 4px",
+            overflow: "hidden",
+            marginBottom: "40px"
+          }}
+        >
+          <div
+            key={`stsy_${name}_knobs_container`}
+            style={{
+              display: "grid",
+              gridTemplateColumns: "25% auto"
+            }}
+          >
+            {Object.values(this.props.knobs).map(v => this.createKnob(v))}
+          </div>
+        </div>
+>>>>>>> parent of 195041b... short story
       </div>
     )
   }
 }
+<<<<<<< HEAD
+=======
+
+ShortStory.defaultProps = {
+  name: "My Element",
+  knobs: {}
+};
+
+export default ShortStory;
+
+const Input = props => <input key="Stsy-input" {...props} />;
+>>>>>>> parent of 195041b... short story
